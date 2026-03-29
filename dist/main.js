e5ebcfc7cc43c13b00e160ede3c74d55e0d9dbbc
renawmontanari <regiangelica@gmail.com>
2026-03-29 11:38:11 -0300

    adicionando as validacoes para email unico e criando o decorator

--- dist/main.js
+++ dist/main.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const common_1 = require("@nestjs/common");
+const class_validator_1 = require("class-validator");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.useGlobalPipes(new common_1.ValidationPipe({
@@ -10,6 +11,7 @@ async function bootstrap() {
         whitelist: true,
         forbidNonWhitelisted: true,
     }));
+    (0, class_validator_1.useContainer)(app.select(app_module_1.AppModule), { fallbackOnErrors: true });
     await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
