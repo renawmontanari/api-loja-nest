a22c4214a847ee34c4b6fb0082435ae3e9f84558
renawmontanari <regiangelica@gmail.com>
2026-03-29 14:51:19 -0300

    criando as rotas para prudutos

---
+++ dist/produto/dto/ListaProduto.dto.js
@@ -0,0 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ListaProdutoDTO = void 0;
+class ListaCaracteristicaProdutoDTO {
+    nome;
+    descricao;
+}
+class ListaImagemProdutoDTO {
+    url;
+    descricao;
+}
+class ListaProdutoDTO {
+    id;
+    usuarioId;
+    nome;
+    valor;
+    quantidade;
+    descricao;
+    categoria;
+    caracteristicas;
+    imagens;
+}
+exports.ListaProdutoDTO = ListaProdutoDTO;
+//# sourceMappingURL=ListaProduto.dto.js.map
---
+++ dist/produto/produto.entity.js
@@ -0,0 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ProdutoEntity = void 0;
+class CaracteristicaProduto {
+    nome;
+    descricao;
+}
+class ImagemProduto {
+    url;
+    descricao;
+}
+class ProdutoEntity {
+    id;
+    usuarioId;
+    nome;
+    valor;
+    quantidade;
+    descricao;
+    categoria;
+    caracteristicas;
+    imagens;
+}
+exports.ProdutoEntity = ProdutoEntity;
+//# sourceMappingURL=produto.entity.js.map
